f2a3ce379ca6f3c87b467790d6a15696e28824d9
Cape23180909 <samilgamer@outlook.com>
2024-08-26 23:53:54 -0400

    Aqui hacemos las inyecciones

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Components;
+using RegistroTecnicos.DAL;
 
 namespace RegistroTecnicos
 {
@@ -11,6 +13,9 @@ namespace RegistroTecnicos
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            var ConStr = builder.Configuration.GetConnectionString("ConStr");
+            builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
 
             var app = builder.Build();
 
