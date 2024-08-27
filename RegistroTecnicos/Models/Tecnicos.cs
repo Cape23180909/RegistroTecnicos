7ebd729f62b5711f7cf16a84f636652ae75c4936
Cape23180909 <samilgamer@outlook.com>
2024-08-26 23:52:15 -0400

    Aqui estamos declarando mis dichas propiedades que va a llevar estos campos

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+        [Required(ErrorMessage = "Llenar este campo por favor.")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Llenar este campo por favor.")]
+        public decimal? Sueldohora { get; set; }
+    }
+}
