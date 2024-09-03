b54f54bbb476a6c527583e5e69ceca722d4f8978
Cape23180909 <samilgamer@outlook.com>
2024-09-03 14:53:00 -0400

    Aqui estamos llamando las nuevas propiedades que va a tener nuestra tabla "Tipos Tecnicos"

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -7,5 +7,6 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options)
         : base(options) { }
     public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
 
 }
