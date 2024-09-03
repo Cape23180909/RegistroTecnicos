57c6b959249c1d9356d809a94591c3de10a4eb7e
Cape23180909 <samilgamer@outlook.com>
2024-09-03 03:04:18 -0400

    Depurando mi codigo por parte para ver como era que se iba a implementar las notificaciones.

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -10,7 +10,6 @@ namespace RegistroTecnicos
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
