effeeee0bf8214f63e8c919380b0d23783f8e6dd
Cape23180909 <samilgamer@outlook.com>
2024-09-04 01:28:01 -0400

    Finalizando el nuevo modelo que en este caso es de "TiposTecnicos" para trabajar con estas dichas propiedades

---
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Models
+{
+    public class TiposTecnicos
+    {
+        [Key]
+        public int TipoId { get; set; }
+        [Required (ErrorMessage ="Coloca una descripcion") ]
+        public string Descripcion { get; set; }
+
+    }
+}
