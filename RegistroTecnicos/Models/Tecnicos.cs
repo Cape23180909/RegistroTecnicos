a8fd9fbef6c8fd9aa08d38f16c60cff47f2ac6a0
Cape23180909 <samilgamer@outlook.com>
2024-09-04 22:12:50 -0400

    Aqui estamos colocando la "ForeignKey" para que este pueda filtrar la informacion de la otra tabla.

--- RegistroTecnicos/Models/Tecnicos.cs
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroTecnicos.Models;
 
@@ -10,4 +11,7 @@ public class Tecnicos
     public string? Nombres { get; set; }
     [Required(ErrorMessage = "Llenar este campo por favor.")]
     public decimal? Sueldohora { get; set; }
+    [ForeignKey("TiposTecnicos")]
+    public int TipoId { get; set; }
+    public TiposTecnicos? TiposTecnicos { get; set; }
 }
