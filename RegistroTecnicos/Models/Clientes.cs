f73d58ef3cab3999bb505800bb42fcab821e65fe
Cape23180909 <samilgamer@outlook.com>
2024-09-10 18:44:55 -0400

    Completando el nuevo modelo de "Clientes" con las dichas propiedades que va a tener el mismo.

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required (ErrorMessage = "Llenar este campo por favor.")]
+        public string Nombres { get; set; }
+        [Required (ErrorMessage = "Llenar este campo por favor.")]
+        public int WhatsApp { get; set; }
+    }
