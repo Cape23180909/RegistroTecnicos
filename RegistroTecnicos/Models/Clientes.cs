de9f1b88bbaf202d75c7e9d419e01de1b018ebea
Cape23180909 <samilgamer@outlook.com>
2024-09-10 21:45:37 -0400

    Colocando el "RegularExpresion" para manejos mas optimos en el campo "WhatsApp" y colocando el tipo de dato que nunca sera nulo.

--- RegistroTecnicos/Models/Clientes.cs
+++ RegistroTecnicos/Models/Clientes.cs
@@ -2,12 +2,13 @@
 
 namespace RegistroTecnicos.Models;
 
-    public class Clientes
-    {
-        [Key]
-        public int ClienteId { get; set; }
-        [Required (ErrorMessage = "Llenar este campo por favor.")]
-        public string Nombres { get; set; }
-        [Required (ErrorMessage = "Llenar este campo por favor.")]
-        public int WhatsApp { get; set; }
-    }
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public string? Nombres { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Ingrese un número de WhatsApp válido (formato requerido: 000-000-0000)")]
+    public string? WhatsApp { get; set; }
+}
