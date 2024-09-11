a708327f794b85d1efac5d2442d46a5508d9598e
Cape23180909 <samilgamer@outlook.com>
2024-09-10 23:15:32 -0400

    Terminando de hacer el modelo de "Trabajos" con las propiedades y ForeignKey necesarias del mismo.

---
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RegistroTecnicos.Models;
+public class Trabajos
+{
+    [Key]
+    public int TrabajoId { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public DateTime? Fecha { get; set; }
+    [ForeignKey("Clientes")]
+    public int? ClienteId { get; set; }
+    [ForeignKey("Tecnicos")]
+    public int? TecnicoId { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public string? Descripcion { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public decimal? Monto { get; set; }
+
+}
