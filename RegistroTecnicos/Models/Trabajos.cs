6158b89bfd4cdd2644c8a6f0ff2bc77f3bd1754f
Cape23180909 <samilgamer@outlook.com>
2024-09-11 22:39:16 -0400

    Colocando las validaciones y las propiedades que lleva dicha ForeignKey

--- RegistroTecnicos/Models/Trabajos.cs
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -7,10 +7,14 @@ public class Trabajos
     public int TrabajoId { get; set; }
     [Required(ErrorMessage = "Llenar este campo por favor.")]
     public DateTime? Fecha { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
     [ForeignKey("Clientes")]
     public int? ClienteId { get; set; }
+    public  Clientes? Clientes { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
     [ForeignKey("Tecnicos")]
     public int? TecnicoId { get; set; }
+    public Tecnicos? Tecnicos { get; set; }
     [Required(ErrorMessage = "Llenar este campo por favor.")]
     public string? Descripcion { get; set; }
     [Required(ErrorMessage = "Llenar este campo por favor.")]
