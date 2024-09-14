38a6a035386f4e36546a830a4bf01a7eab3c23f5
Cape23180909 <samilgamer@outlook.com>
2024-09-13 22:42:24 -0400

    Eliminando la propiedad de la entidad trabajo ya que aqui no uso Forkhey de momento.

--- RegistroTecnicos/Models/Clientes.cs
+++ RegistroTecnicos/Models/Clientes.cs
@@ -9,8 +9,5 @@ public class Clientes
     [Required(ErrorMessage = "Llenar este campo por favor.")]
     public string? Nombres { get; set; }
     [Required(ErrorMessage = "Llenar este campo por favor.")]
-    [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Ingrese un número de WhatsApp válido (formato requerido: 000-000-0000)")]
     public string? WhatsApp { get; set; }
-
-    public Trabajos? Trabajos { get; set; }
-}
+} 
