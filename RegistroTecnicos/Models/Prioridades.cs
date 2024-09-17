6b37ba9189158da3e0eac73b110801add2288d64
Cape23180909 <samilgamer@outlook.com>
2024-09-17 08:39:48 -0400

    Terminando de hacer mi unevo modelo que es prioridades

---
+++ RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public string? Descripcion { get; set; }
+    [Required(ErrorMessage = "Llenar este campo por favor.")]
+    public double? Tiempo { get; set; }
+}
