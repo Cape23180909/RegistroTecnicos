5279c08317e344b5a0b03eb26ba2a5a25ece1cca
Cape23180909 <samilgamer@outlook.com>
2024-10-18 03:05:23 -0400

    Injectando el articuloService del mismo.

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -24,6 +24,7 @@ namespace RegistroTecnicos
             builder.Services.AddScoped<ClienteService>();
             builder.Services.AddScoped<TrabajoService>();
             builder.Services.AddScoped<PrioridadService>();
+            builder.Services.AddScoped<ArticuloService>();
 
             var app = builder.Build();
 
