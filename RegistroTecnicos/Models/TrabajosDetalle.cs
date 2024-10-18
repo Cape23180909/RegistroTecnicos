e66282977c428fd49fb95fe2668d8f18ea616d0d
Cape23180909 <samilgamer@outlook.com>
2024-10-18 00:04:54 -0400

    Terminando de hacer el modelo de trabajoDetalle.

---
+++ RegistroTecnicos/Models/TrabajosDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int TrabajoId { get; set; }
+
+    [ForeignKey("TrabajoId")]
+    public Trabajos Trabajo { get; set; }
+
+    public int ArticuloId { get; set; }
+
+    [ForeignKey("ArticuloId")]
+    public Articulos Articulo { get; set; }
+
+    [Required(ErrorMessage = "Favor Colocar la cantidad.")]
+    public decimal Cantidad { get; set; }
+    [Required(ErrorMessage = "Favor Colocar el precio.")]
+    public decimal Precio { get; set; }
+    [Required(ErrorMessage = "Favor Colocar el costo.")]
+    public decimal Costo { get; set; }
+
+}
